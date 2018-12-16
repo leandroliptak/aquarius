49d4feb1a88658b87d584179fdc48e05a7a9fdd3
Leandro Liptak <lliptak@keepcon.com>
2018-12-16 10:40:42 -0300

    .

--- src/instabot.py
+++ src/instabot.py
@@ -437,18 +437,19 @@ class InstaBot:
 
     def get_username_by_user_id(self, user_id):
         """ Get username by user_id """
-        if self.login_status:
-            try:
-                url_info = self.api_user_detail % user_id
-                r = self.s.get(url_info, headers="")
-                all_data = json.loads(r.text)
-                username = all_data["user"]["username"]
-                return username
-            except:
-                logging.exception("Except on get_username_by_user_id")
-                return False
-        else:
-            return False
+        return "."
+#        if self.login_status:
+#            try:
+#                url_info = self.api_user_detail % user_id
+#                r = self.s.get(url_info, headers="")
+#                all_data = json.loads(r.text)
+#                username = all_data["user"]["username"]
+#                return username
+#            except:
+#                logging.exception("Except on get_username_by_user_id")
+#                return False
+#        else:
+#            return False
 
     def get_userinfo_by_name(self, username):
         """ Get user info by name """
